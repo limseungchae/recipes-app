b475d33ad0c95e823f587b0fccfcededdf6e26ec
limseungchae <tmdco12@naver.com>
2023-03-02 12:58:40 +0900

    recipe-app v2 : Recipe ->  IngredientsList, Instructions

--- src/components/Recipe.js
+++ src/components/Recipe.js
@@ -1,21 +1,20 @@
 import React from "react";
+import IngredientsList from "./IngredientsList";
+import Ingredient from "./Ingredient";
+import Instructions from "./Instructions";
 
 const Recipe = ({ name, ingredients, steps }) => {
+    let fname = name.toLowerCase().replace(/ /g, '-');
     return (
-        <section id="baked-salmon">
-            <h1>{name}</h1>
-            <ul className="ingredients">
-                {ingredients.map((ingredient, i) => (
-                    <li key={i}>{ingredient.name}</li>
-                ))}
-            </ul>
-            <section className="instructions">
-                <h2>요리 방법</h2>
-                {steps.map((step, i) => (
-                    <p key={i}>{step}</p>
-                ))}
+        <React.StrictMode>
+            <section id={fname}>
+                <h1>{name}</h1>
+            <div><img src={`./images/${fname}.png`} /></div>
+                <IngredientsList list={ingredients} />
+                <Instructions title="요리 방법" steps={steps} />
             </section>
-        </section>
+        </React.StrictMode>
+
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -4,5 +4,8 @@ import Menu from "./components/Menu";
 // import data from "./data/recipes_ko.json";
 import data from "./data/recipes.json";
 
-ReactDOM.render(<Menu recipes={data} />,
+ReactDOM.render(
+    <React.StrictMode>
+        <Menu recipes={data} />
+    </React.StrictMode>,
     document.getElementById("root"));
